bbfd6b6d74e71bc499d37ad0479c0a636246f22a
furamingo0720 <ftanke0720@gmail.com>
2026-02-06 20:36:22 +0900

    Update heartbeat_publisher_component.cpp

--- protolink_drivers/src/heartbeat_publisher_component.cpp
+++ protolink_drivers/src/heartbeat_publisher_component.cpp
@@ -20,17 +20,37 @@ namespace protolink_drivers
 HeartBeatPublisherComponent::HeartBeatPublisherComponent(const rclcpp::NodeOptions & options)
 : Node("heartbeat_publisher_component", options),
   data_(
-    hardware_communication_msgs::build<hardware_communication_msgs::msg::HeartBeat>().sequence(1)),
+    hardware_communication_msgs::build<hardware_communication_msgs::msg::GroundStationHeartBeat>().sequence(1)),
   params_(heartbeat_publisher::ParamListener(get_node_parameters_interface()).get_params()),
   sock_(protolink::udp_protocol::create_socket(io_context_, params_.from_port)),
   protolink_publisher_(sock_, params_.ip_address, params_.port, this->get_logger()),
   publish_timer_(
     create_wall_timer(std::chrono::duration<double>(1.0 / params_.publish_rate), [&]() {
-      protolink_publisher_.send(protolink__hardware_communication_msgs__HeartBeat::convert(data_));
+      protolink_publisher_.send(protolink__hardware_communication_msgs__GroundStationHeartBeat::convert(data_));
       data_.sequence = data_.sequence + 1;
     }))
 {
+  rcin_sub = this->create_subscription<mavros_msgs::msg::RCIn>(
+  "/mavros/rc/in", 10, std::bind(&HeartBeatPublisherComponent::rcInCallback, this, std::placeholders::_1));
 }
-}  // namespace protolink_drivers
+// namespace protolink_drivers
+
+void HeartBeatPublisherComponent::rcInCallback(const mavros_msgs::msg::RCIn::SharedPtr msg)
+{
+  // 例: プロポの 6ch (インデックスは 5) に割り当てたスイッチで mode を変更
+  // channelsのサイズチェック（念のため）
+  if (msg->channels.size() < 6) return;
+
+  uint16_t sw_value = msg->channels[4]; // 6chの値を取得
+
+  if (sw_value < 1300) {
+    data_.mode = 1; // スイッチが下のとき
+  } else if (sw_value > 1700) {
+    data_.mode = 0; // スイッチが上のときsource /opt/ros/humble/setup.bash
+  } else {
+    data_.mode = 3; // スイッチが真ん中のとき（3ポジションスイッチの場合）
+  }
+}
+}
 
 RCLCPP_COMPONENTS_REGISTER_NODE(protolink_drivers::HeartBeatPublisherComponent)
